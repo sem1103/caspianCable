60a45dd92bca9889193107e1051fdd22931cac97
DESKTOP-BVVFQ66\ACER <renarus>
2024-11-06 21:00:45 +0400

    about page

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import s from './about.module.css'
+import Link from 'next/link'
 const About = () => {
   return (
     <div className={s.about_page}>
@@ -20,6 +23,76 @@ const About = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
           </div>
         </div>
+      </section>
+      <section className={s.about_section_2}>
+        <div className={s.about_mission}>
+       <div className={s.mission_box}>
+        <div className={s.boxes}></div>
+       <img src="/assets/img/missiya.png" alt="" />
+       <h6 className={s.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </h6>
+       </div>
+        <div className={s.mission_box}>
+        <div className={s.boxes}></div>
+        <img src="/assets/img/missiya 2.png" alt="" />
+        <h5 className={s.vision}>Vizyon</h5>
+        </div>
+        </div>
+      </section>
+      <section className={s.about_section_3} >
+      <Swiper
+            className={s.partners__slider}
+            spaceBetween={50}
+            slidesPerView={2}
+            loop={true}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => {             
+              swiperRef.current = swiper;
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+          >
+            <SwiperSlide className={s.slide}>
+              <Link href='/'>
+                <img src="/assets/img/partner1.png" alt="" />
+              </Link>
+            </SwiperSlide>
+
+            <SwiperSlide className={s.slide}>
+              <Link href='/'>
+                <img src="/assets/img/partner2.png" alt="" />
+              </Link>
+            </SwiperSlide>
+
+            <SwiperSlide className={s.slide}>
+              <Link href='/'>
+                <img src="/assets/img/partner3.png" alt="" />
+              </Link>
+            </SwiperSlide>
+
+            <SwiperSlide className={s.slide}>
+              <Link href='/'>
+                <img src="/assets/img/partner4.png" alt="" />
+              </Link>
+            </SwiperSlide>
+
+            <SwiperSlide className={s.slide}>
+              <Link href='/'>
+                <img src="/assets/img/partner2.png" alt="" />
+              </Link>
+            </SwiperSlide>
+          </Swiper>
       </section>
     </div>
   )
